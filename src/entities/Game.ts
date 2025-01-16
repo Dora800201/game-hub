8c4bb9892bcb34aa1157ef8c0a86a7893b60e1bd
dora800201 <dora800201@hotmail.com>
2025-01-16 13:27:48 +1100

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publishers";
 
 export interface Game {
   id: number;
@@ -6,6 +8,8 @@ export interface Game {
   background_image: string;
   description_raw: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   parent_platforms: {
     platform: Platform;
   }[];

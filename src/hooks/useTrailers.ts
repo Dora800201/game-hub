90ee77e1e864feaa3c60d0d7b25a89e3b8134ed4
dora800201 <dora800201@hotmail.com>
2025-01-16 16:22:44 +1100

    Refactor  entity interface

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -1,9 +1,7 @@
-import genres from "../data/genres";
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import ms from "ms";
-import { Genre } from "../entities/Genre";
-import { Trailer } from "../entities/Trailer";
+import Trailer from "../entities/Trailer";
 
 const useTrailers = (gameId: number) => {
   const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
